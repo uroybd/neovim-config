960457eb369be15d3a5c26dfc9fd3556b4bb52de
Utsob Roy <roy@utsob.me>
2024-05-09 16:04:31 +0600

    feat(diagnostics): trouble.nvim integration

---
+++ lua/user/extras/trouble.lua
@@ -0,0 +1,27 @@
+local M = {
+"folke/trouble.nvim",
+ dependencies = { "nvim-tree/nvim-web-devicons" },
+  event = "VeryLazy",
+}
+
+M.config = function()
+  local wk = require "which-key"
+  wk.register {
+    ["<leader>dt"] = { "<cmd>TroubleToggle<CR>", "Trouble" },
+    ["<leader>dd"] = { "<cmd>TroubleToggle document_diagnostics<CR>", "Document Diagnostics" },
+    ["<leader>dw"] = { "<cmd>TroubleToggle workspace_diagnostics<CR>", "Workspace Diagnostics" },
+    ["<leader>dr"] = { "<cmd>TroubleToggle lsp_references<CR>", "References" },
+    ["<leader>dq"] = { "<cmd>TroubleToggle quickfix<CR>", "Quickfix" },
+  }
+
+  require("trouble").setup {
+    position = "bottom",
+    height = 15,
+    icons = true,
+    mode = "workspace_diagnostics",
+    group = true,
+    padding = true,
+  }
+end
+
+return M
