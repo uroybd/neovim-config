d8663fc1fae4466dd919a99ba976c5a249afaf59
Utsob Roy <roy@utsob.me>
2024-05-12 15:53:20 +0600

    fix(config): typo

--- lua/user/extras/emmet.lua
+++ lua/user/extras/emmet.lua
@@ -4,7 +4,7 @@ local M = {
 }
 
 M.config = function()
- local wk =require require "which-key"
+ local wk = require("which-key")
  wk.register{
   ["<leader>xe"] = { "<cmd>require('nvim-emmet').wrap_with_abbreviation<cr>", "Emmet Wrap with Abbreviation"}
 } 
