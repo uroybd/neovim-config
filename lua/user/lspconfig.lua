abb1d3509e1bb4a4f83fa925a3c9207402d1ae9e
Utsob Roy <roy@utsob.me>
2024-05-20 08:43:56 +0600

    feat(plugin): time-tracker

--- lua/user/lspconfig.lua
+++ lua/user/lspconfig.lua
@@ -21,7 +21,7 @@ M.on_attach = function(client, bufnr)
   lsp_keymaps(bufnr)
 
   if client.supports_method "textDocument/inlayHint" then
-    vim.lsp.inlay_hint.enable(bufnr, true)
+    vim.lsp.inlay_hint.enable(true, {bufnr = bufnr})
   end
 end
 
@@ -33,7 +33,7 @@ end
 
 M.toggle_inlay_hints = function()
   local bufnr = vim.api.nvim_get_current_buf()
-  vim.lsp.inlay_hint.enable(bufnr, not vim.lsp.inlay_hint.is_enabled(bufnr))
+  vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled(bufnr), { bufnr = bufnr })
 end
 
 function M.config()
