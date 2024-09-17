8b7f509f2c1d1ca6e31af46cebf7c5e2a1fd7d8b
Utsob Roy <roy@utsob.me>
2024-09-17 10:17:00 +0600

    feat(tabby): more keymaps

--- lua/user/extras/tabby.lua
+++ lua/user/extras/tabby.lua
@@ -10,6 +10,16 @@ function M.config()
   local wk = require "which-key"
   wk.add {
     { "<leader>aj", "<cmd>Tabby jump_to_tab<cr>", desc = "Jump to tab" },
+    {
+      "<leader>ar",
+      function()
+        local tab_name = vim.fn.input("Tab name", "")
+        if tab_name then
+          require("tabby").tab_rename(tab_name)
+        end
+      end,
+      desc = "Rename tab",
+    },
   }
 end
 
