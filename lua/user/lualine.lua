38fcf4f53232d1fa7cae72356cfe04882692f594
Utsob Roy <roy@utsob.me>
2024-09-29 09:53:46 +0600

    fix(neovim): fix nerd font rendering in alacritty

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -1,14 +1,13 @@
 local M = {
   "nvim-lualine/lualine.nvim",
-  dependencies = {
-  },
+  dependencies = {},
 }
 
 function M.config()
   require("lualine").setup {
     options = {
       component_separators = { left = "", right = "" },
-      section_separators = { left = " ", right = " " },
+      section_separators = { left = "", right = "" },
 
       ignore_focus = { "NvimTree" },
       theme = "catppuccin",
