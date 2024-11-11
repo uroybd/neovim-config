68f3c6e294438a5e4d36572ea6a6eba209ddceda
Utsob Roy <roy@utsob.me>
2024-11-11 22:09:06 +0600

    feat(nvim): blink tweaks

--- lua/user/blink.lua
+++ lua/user/blink.lua
@@ -22,9 +22,19 @@ function M.config()
     -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
     -- adjusts spacing to ensure icons are aligned
     nerd_font_variant = "mono",
+    accept = {
+      auto_brackets = {
+        enabled = true,
+      },
+    },
     trigger = {
       signature_help = {
         enabled = true,
+      },
+    },
+    windows = {
+      documentation = {
+        auto_show = true,
       },
     },
   }
