ccc51e4688ac6a1370e5d07218955c5ec3ec16b4
Utsob Roy <roy@utsob.me>
2024-12-05 17:17:30 +0600

    fix(neovim): blink cmp fix

--- lua/user/blink.lua
+++ lua/user/blink.lua
@@ -2,7 +2,9 @@ local M = {
   "saghen/blink.cmp",
   lazy = false, -- lazy loading handled internally
   -- optional: provides snippets for the snippet source
-  dependencies = "rafamadriz/friendly-snippets",
+  dependencies = {
+    { "L3MON4D3/LuaSnip", dependencies = { "rafamadriz/friendly-snippets" } },
+  },
 
   -- use a release tag to download pre-built binaries
   version = "v0.*",
@@ -10,28 +12,45 @@ local M = {
 }
 
 function M.config()
+  -- require("luasnip").setup {}
+  require("luasnip.loaders.from_vscode").lazy_load()
   require("blink.cmp").setup {
-
+    snippets = {
+      expand = function(snippet)
+        require("luasnip").lsp_expand(snippet)
+      end,
+      active = function(filter)
+        if filter and filter.direction then
+          return require("luasnip").jumpable(filter.direction)
+        end
+        return require("luasnip").in_snippet()
+      end,
+      jump = function(direction)
+        require("luasnip").jump(direction)
+      end,
+    },
+    sources = {
+      completion = {
+        enabled_providers = { "lsp", "path", "luasnip", "buffer" },
+      },
+    },
     keymap = { preset = "enter" },
 
-    highlight = {
+    appearance = {
       -- sets the fallback highlight groups to nvim-cmp's highlight groups
       -- useful for when your theme doesn't support blink.cmp
       -- will be removed in a future release, assuming themes add support
       use_nvim_cmp_as_default = true,
     },
-    -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-    -- adjusts spacing to ensure icons are aligned
-    nerd_font_variant = "mono",
-    accept = {
-      auto_brackets = {
-        enabled = true,
+    completion = {
+      accept = {
+        auto_brackets = {
+          enabled = true,
+        },
       },
     },
-    windows = {
-      documentation = {
-        auto_show = true,
-      },
+    documentation = {
+      auto_show = true,
     },
   }
 end
