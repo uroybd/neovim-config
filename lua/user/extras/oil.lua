9e4d9dae57f6a6202468b51690a7b561dc71e878
Utsob Roy <roy@utsob.me>
2025-01-06 17:35:26 +0600

    perf(neovim): tweak oil.nvim

--- lua/user/extras/oil.lua
+++ lua/user/extras/oil.lua
@@ -4,12 +4,26 @@ local M = {
 }
 
 function M.config()
+  local detail = false
   require("oil").setup {
     float = {
       max_height = 20,
-      max_width = 60,
+      max_width = 100,
     },
     delete_to_trash = true,
+    keymaps = {
+      ["gd"] = {
+        desc = "Toggle file detail view",
+        callback = function()
+          detail = not detail
+          if detail then
+            require("oil").set_columns { "icon", "permissions", "size", "mtime" }
+          else
+            require("oil").set_columns { "icon" }
+          end
+        end,
+      },
+    },
   }
   vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
 end
