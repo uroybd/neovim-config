00c88c46530ff59f64e5394bc7151eabd0ab2c75
Utsob Roy <roy@utsob.me>
2025-01-10 01:02:37 +0600

    fix(neovim): gopls config

--- lua/user/lspsettings/gopls.lua
+++ lua/user/lspsettings/gopls.lua
@@ -1,25 +1,6 @@
 return {
   settings = {
     gopls = {
-      codelenses = {
-        gc_details = false,
-        generate = true,
-        regenerate_cgo = true,
-        run_govulncheck = true,
-        test = true,
-        tidy = true,
-        upgrade_dependency = true,
-        vendor = true,
-      },
-      hints = {
-        assignVariableTypes = true,
-        compositeLiteralFields = true,
-        compositeLiteralTypes = true,
-        constantValues = true,
-        functionTypeParameters = true,
-        parameterNames = true,
-        rangeVariableTypes = true,
-      },
       analyses = {
         fieldalignment = true,
         nilness = true,
@@ -30,11 +11,6 @@ return {
         waitgroup = true,
         modernize = true,
       },
-      usePlaceholders = true,
-      completeUnimported = true,
-      staticcheck = true,
-      directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules", "-.nvim" },
-      semanticTokens = true,
     },
   },
 }
