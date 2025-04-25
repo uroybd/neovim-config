937ff817e1dd7b8885e90fa94a60cdcb90a1915e
Utsob Roy <roy@utsob.me>
2025-04-25 13:36:55 +0600

    feat(neovim): copilot and copilot chat

--- lua/user/copilot-chat.lua
+++ lua/user/copilot-chat.lua
@@ -1,14 +1,34 @@
-return {
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    dependencies = {
-      { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
-      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
-    },
-    build = "make tiktoken", -- Only on MacOS or Linux
-    opts = {
-      -- See Configuration section for options
-    },
-    -- See Commands section for default commands if you want to lazy load on them
+local M = {
+  "CopilotC-Nvim/CopilotChat.nvim",
+  dependencies = {
+    { "zbirenbaum/copilot.lua" },
+    { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+  },
+  build = "make tiktoken", -- Only on MacOS or Linux
+  cmd = {
+    "CopilotChat",
+    "CopilotChatToggle",
+    "CopilotChatPrompts",
+    "CopilotChatAgents",
+    "CopilotChatModels",
   },
 }
+
+function M.config()
+  require("CopilotChat").setup {
+    sticky = {
+      "#files",
+    },
+  }
+  local wk = require "which-key"
+  wk.add {
+    { "<leader>C", group = "Copilot Chat", icon = "" },
+    { "<leader>Cc", "<cmd>CopilotChat<cr>", desc = "Ask Copilot", icon = "󰭹" },
+    { "<leader>Ct", "<cmd>CopilotChatToggle<cr>", desc = "Toggle Chat" },
+    { "<leader>Cp", "<cmd>CopilotChatPrompts<cr>", desc = "Prompt templates" },
+    { "<leader>Ca", "<cmd>CopilotChatAgents<cr>", desc = "Select Agent" },
+    { "<leader>Cm", "<cmd>CopilotChatModels<cr>", desc = "Select Model" },
+  }
+end
+
+return M
