5cae73b263340c8e7546c6a4b4b59f80a2b6ddde
Utsob Roy <roy@utsob.me>
2025-06-12 14:19:47 +0600

    feat(nvim): nvim-window home-row only

--- lua/user/extras/nvim-window.lua
+++ lua/user/extras/nvim-window.lua
@@ -5,7 +5,21 @@ local M = {
 
 function M.config()
   local nvim_window = require "nvim-window"
-  nvim_window.setup { normal_hl = "@comment.warning", border = "none" }
+  nvim_window.setup {
+    normal_hl = "@comment.warning",
+    border = "none",
+    chars = {
+      "a",
+      "s",
+      "d",
+      "f",
+      "g",
+      "h",
+      "j",
+      "k",
+      "l",
+    },
+  }
   local wk = require "which-key"
   wk.add {
     {
