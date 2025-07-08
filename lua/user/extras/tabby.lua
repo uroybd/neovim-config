d243e0ce70df5ee12b65aeb779e1825189f54083
Utsob Roy <roy@utsob.me>
2025-07-08 18:42:57 +0600

    feat(ghostty,neovim): style update

--- lua/user/extras/tabby.lua
+++ lua/user/extras/tabby.lua
@@ -21,17 +21,17 @@ function M.config()
       return {
         {
           { "  ", hl = theme.head },
-          line.sep(" ", theme.head, theme.fill),
+          line.sep("", theme.head, theme.fill),
         },
         line.tabs().foreach(function(tab)
           local hl = tab.is_current() and theme.current_tab or theme.tab
           return {
-            line.sep(" ", hl, theme.fill),
+            line.sep("", hl, theme.fill),
             tab.is_current() and " " or "󰆣 ",
             tab.number(),
             tab.name(),
             tab.close_btn "",
-            line.sep(" ", hl, theme.fill),
+            line.sep("", hl, theme.fill),
             hl = hl,
             margin = " ",
           }
@@ -40,16 +40,16 @@ function M.config()
         line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
           local hl = win.is_current() and theme.current_win or theme.win
           return {
-            line.sep(" ", hl, theme.fill),
+            line.sep("", hl, theme.fill),
             win.is_current() and " " or " ",
             win.buf_name(),
-            line.sep(" ", hl, theme.fill),
+            line.sep("", hl, theme.fill),
             hl = hl,
             margin = " ",
           }
         end),
         {
-          line.sep(" ", theme.tail, theme.fill),
+          line.sep("", theme.tail, theme.fill),
           { "  ", hl = theme.tail },
         },
         hl = theme.fill,
