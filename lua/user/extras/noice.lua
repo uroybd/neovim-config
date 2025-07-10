b48ebb8ce07a0b68bb004b011709964c21cebeeb
Utsob Roy <roy@utsob.me>
2025-07-10 20:30:41 +0600

    feat(nvim): noice again

--- lua/user/extras/noice.lua
+++ lua/user/extras/noice.lua
@@ -3,7 +3,6 @@ local M = {
   event = "VeryLazy",
   dependencies = {
     "MunifTanjim/nui.nvim",
-    "rcarriga/nvim-notify",
   },
 }
 
@@ -21,6 +20,14 @@ function M.config()
       long_message_to_split = true, -- long messages will be sent to a split
       inc_rename = false, -- enables an input dialog for inc-rename.nvim
       lsp_doc_border = false, -- add a border to hover docs and signature help
+    },
+    views = {
+      cmdline_popup = {
+        position = {
+          row = "30%",
+          col = "50%",
+        },
+      },
     },
   }
 end
