fe037e496d55b43d3e3e3ef57ed26d54eaf0c0fa
Utsob Roy <roy@utsob.me>
2026-02-10 16:50:29 +0600

    feat(nvim): use forked newer project.nvim

--- lua/user/project.lua
+++ lua/user/project.lua
@@ -1,19 +1,8 @@
 local M = {
-  "ahmedkhalf/project.nvim",
+  "DrKJeff16/project.nvim",
   event = "VeryLazy",
 }
 function M.config()
-  require("project_nvim").setup {
-    active = true,
-    on_config_done = nil,
-    manual_mode = false,
-    detection_methods = { "pattern" },
-    patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", "pom.xml", "go.mod" },
-    ignore_lsp = {},
-    exclude_dirs = {},
-    show_hidden = false,
-    silent_chdir = true,
-    scope_chdir = "global",
-  }
+  require("project").setup {}
 end
 return M
