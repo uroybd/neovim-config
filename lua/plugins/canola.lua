62f58798c19e665d6adf3764188f8d6cfeba7d7e
Utsob Roy <roy@utsob.me>
2026-05-05 16:17:12 +0600

    fix(neovim): migrate to canola

---
+++ lua/plugins/canola.lua
@@ -0,0 +1,30 @@
+local M = {
+	"barrettruth/canola.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons", "barrettruth/canola-collection" },
+	keys = {
+		{ "-", "<CMD>Canola --float<CR>", desc = "Open parent directory" },
+	},
+	branch = "canola",
+}
+
+vim.g.canola = {
+	columns = { "icon" },
+	cursor = true,
+	float = {
+		border = "single",
+		max_height = 20,
+		max_width = 100,
+	},
+	hidden = {
+		enabled = true,
+	},
+	keymaps = {
+		gd = {
+			desc = "Toggle file detail view",
+		},
+	},
+	watch = false,
+}
+vim.g.canola_trash = {}
+
+return M
