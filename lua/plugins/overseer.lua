4cd301ead4126127e0b4c39c422447947192e45a
Utsob Roy <roy@utsob.me>
2026-06-11 22:37:02 +0600

    feat(nvim): open overseer panel before running test

--- lua/plugins/overseer.lua
+++ lua/plugins/overseer.lua
@@ -1,20 +1,19 @@
 local M = {
-  "stevearc/overseer.nvim",
-  event = "VeryLazy",
-  opts = {},
+	"stevearc/overseer.nvim",
+	event = "VeryLazy",
 }
 
-function M.config(_, opts)
-  require("overseer").setup(opts)
+function M.config()
+	require("overseer").setup()
 
-  local wk = require("which-key")
-  wk.add({
-    { "<leader>T", group = "Tasks" },
-    { "<leader>Tt", "<cmd>OverseerToggle<CR>", desc = "Toggle" },
-    { "<leader>Tr", "<cmd>OverseerRun<CR>", desc = "Run" },
-    { "<leader>Tn", "<cmd>OverseerRun TestNearest<CR>", desc = "Test Nearest" },
-    { "<leader>Tf", "<cmd>OverseerRun TestFile<CR>", desc = "Test File" },
-  })
+	local wk = require("which-key")
+	wk.add({
+		{ "<leader>T", group = "Tasks" },
+		{ "<leader>Tt", "<cmd>OverseerToggle!<CR>", desc = "Toggle" },
+		{ "<leader>Tr", "<cmd>OverseerOpen!<CR><cmd>OverseerRun<CR>", desc = "Run" },
+		{ "<leader>Tn", "<cmd>OverseerOpen!<CR><cmd>OverseerRun TestNearest<CR>", desc = "Test Nearest" },
+		{ "<leader>Tf", "<cmd>OverseerOpen!<CR><cmd>OverseerRun TestFile<CR>", desc = "Test File" },
+	})
 end
 
 return M
