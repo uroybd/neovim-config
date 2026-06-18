c022cb471ac18e6858984d75439a8aaebe5ec964
Utsob Roy <roy@utsob.me>
2026-06-18 20:09:48 +0600

    feat(nvim): theme tweaks

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -11,7 +11,16 @@ function M.config()
 			dark = "mist",
 			light = "pearl",
 		},
+		keywordStyle = { italic = true, bold = true },
 		dimInactive = true,
+		overrides = function(colors)
+			-- Log colors to file
+			return {
+				LineNrAbove = { fg = colors.palette.green2 },
+				LineNrBelow = { fg = colors.palette.blue2 },
+				["@module"] = { fg = colors.palette.yellow },
+			}
+		end,
 	})
 	vim.cmd.colorscheme("kanso")
 end
