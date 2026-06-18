0d851c04fd41a688863614c954b3604ca449ebdb
Utsob Roy <roy@utsob.me>
2026-06-18 21:38:04 +0600

    chore(nvim): remove leftover config

--- lua/plugins/modicator.lua
+++ lua/plugins/modicator.lua
@@ -1,33 +1,24 @@
 local M = {
-  "mawkler/modicator.nvim",
-  event = "BufEnter",
-  opts = {
-    show_warnings = false,
-    highlights = {
-      defaults = {
-        bold = true,
-        italic = true,
-      },
-      use_cursorline_background = true,
-    },
-  },
+	"mawkler/modicator.nvim",
+	event = "BufEnter",
+	opts = {
+		show_warnings = false,
+		highlights = {
+			defaults = {
+				bold = true,
+				italic = true,
+			},
+			use_cursorline_background = true,
+		},
+	},
 }
 
-function M.init()
-  vim.o.cursorline = true
-  vim.o.number = true
-  vim.o.termguicolors = true
-  vim.o.relativenumber = true
-end
-
 function M.config(_, opts)
-  require("modicator").setup(opts)
-
-  vim.api.nvim_create_autocmd({ "Colorscheme" }, {
-    callback = function()
-      vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#388bfd", bg = "NONE" })
-    end,
-  })
+	vim.o.cursorline = true
+	vim.o.number = true
+	vim.o.termguicolors = true
+	vim.o.relativenumber = true
+	require("modicator").setup(opts)
 end
 
 return M
