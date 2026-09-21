fcae90803135cb99494affac618cb1404f37e9ad
Utsob Roy <roy@utsob.me>
2026-09-21 18:16:22 +0600

    feat(nvim): refresh diffview via gitsigns if sidekick updates code

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,45 +1,48 @@
 local M = {
-  "lewis6991/gitsigns.nvim",
-  event = "BufEnter",
+	"lewis6991/gitsigns.nvim",
+	event = "BufEnter",
 }
 
 function M.config()
-  local icons = require("user.icons")
+	local icons = require("user.icons")
 
-  require("gitsigns").setup({
-    signs = {
-      add = {
-        text = icons.ui.BoldLineMiddle,
-      },
-      change = {
-        text = icons.ui.BoldLineDashedMiddle,
-      },
-      delete = {
-        text = icons.ui.TriangleShortArrowRight,
-      },
-      topdelete = {
-        text = icons.ui.TriangleShortArrowRight,
-      },
-      changedelete = {
-        text = icons.ui.BoldLineMiddle,
-      },
-    },
-    watch_gitdir = {
-      interval = 1000,
-      follow_files = true,
-    },
-    attach_to_untracked = true,
-    current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
-    update_debounce = 200,
-    max_file_length = 40000,
-    preview_config = {
-      border = "rounded",
-      style = "minimal",
-      relative = "cursor",
-      row = 0,
-      col = 1,
-    },
-  })
+	require("gitsigns").setup({
+		signs = {
+			add = {
+				text = icons.ui.BoldLineMiddle,
+			},
+			change = {
+				text = icons.ui.BoldLineDashedMiddle,
+			},
+			delete = {
+				text = icons.ui.TriangleShortArrowRight,
+			},
+			topdelete = {
+				text = icons.ui.TriangleShortArrowRight,
+			},
+			changedelete = {
+				text = icons.ui.BoldLineMiddle,
+			},
+		},
+		watch_gitdir = {
+			interval = 1000,
+			follow_files = true,
+		},
+		watch_index = {
+			interval = 1000,
+		},
+		attach_to_untracked = true,
+		current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+		update_debounce = 200,
+		max_file_length = 40000,
+		preview_config = {
+			border = "rounded",
+			style = "minimal",
+			relative = "cursor",
+			row = 0,
+			col = 1,
+		},
+	})
 end
 
 return M
